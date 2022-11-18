6fa1ea499fd594a571570fb6d957a51d1bd4d5d4
Vaibhav <vaibhavshahi359@gmail.com>
2022-11-18 17:30:07 +0530

    push

---
+++ Day 001/Conditions.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Conditions {
+    public static void main(String[] args) {
+        System.out.println("Enter the Marks of Student");
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("Excellent");
+        } else {
+            if (90<< marks >= 80) {
+                System.out.println("Good");
+            } else if (80<< marks >= 70) {
+                System.out.println("Fair");
+
+            } else {
+                if (70<< marks >= 60) {
+                    System.out.println("Meet Exepectations");
+                } else
+                {
+                    System.out.println("Below Average");
+                }
+            }
+        }
+    }
+} 
---
+++ Day 001/Loops.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Loops {
+
+    public static void main(String[] args) {
+
+//        int i = 0;
+//        while (i <= 20) {
+//            System.out.println(i);
+//            i = i + 2;
+//        }
+//         for (int i = 0; i <=20 ; i=i+1) {
+//            System.out.println(i);
+//        }
+        int i=2;
+        do {
+            System.out.println( i);
+            i = i+1;
+        } while (i<8);
+
+
+
+    }
+}
