6a8e5fc04c1bbacb6df37a10a21782a2290bc309
Vaibhav <vaibhavshahi359@gmail.com>
2022-11-20 01:05:25 +0530

    modified

--- Day 001/input.java
+++ Day 001/input.java
@@ -3,7 +3,7 @@ public class input {
     public static void main(String[] args){
       System.out.println("Enter the desired number");
       Scanner sc = new Scanner(System.in);
-    System.out
+    
       int n = sc.nextInt();
       String name = sc.nextLine();
         System.out.print("Dear"+name+" Here is the counting"+" " );
