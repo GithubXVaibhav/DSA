e9128762bd8955d20a9a4bc0f3716310cb75b411
Vaibhav <vaibhavshahi359@gmail.com>
2022-11-20 12:09:20 +0530

    no  modifications

---
+++ Day 002/PrimeNumbersInRange.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class PrimeNumbersInRange {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int low = sc.nextInt();
+        int high = sc.nextInt();
+        System.out.println("these are  the prime numbers between "+low+" and "+high );
+        for (int i = low + 1; i < high; i++) {
+            int count = 0;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+           if(count==0)
+           System.out.println(i);
+        }
+
+    }
+}
